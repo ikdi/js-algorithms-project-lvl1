410e96408816949cb19d648928c92d9c4344e190
KDI <perecd@ukr.net>
2021-03-07 15:18:52 +0200

    step2

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 const buildSearchEngine = (documents) => {
   const index = documents.reduce((acc, document) => {
     const { id, text } = document;
-    const words = text
-      .split(/\W/)
-      .filter((word) => word !== '');
+    const words = text.match(/\w+/g) ?? [];
+      // .split(' ')
+      // .filter((word) => word.length > 0)
+      // .map((word) => .test());
     
-    // console.log(id, words);
+    console.log(id, words);
 
     return words.reduce((accWords, word) => {
       if (!accWords[word]) {
@@ -20,19 +21,22 @@ const buildSearchEngine = (documents) => {
 
   return {
     search(word) {
-      return index[word];    
+      const [ term ] = word.match(/\w+/g);
+      return index[term];    
     }
   }
 };
 
 const doc1 = { id: 'doc1', text: "I can't shoot straight unless I've had a pint!" };
-const doc2 = { id: 'doc2', text: "Don't shoot shoot shoot that thing at me." };
-const doc3 = { id: 'doc3', text: "I'm your shooter." };
-const docs = [doc1, doc2, doc3];
+// const doc2 = { id: 'doc2', text: "Don't shoot shoot shoot that thing at me." };
+// const doc3 = { id: 'doc3', text: "I'm your shooter." };
+// const docs = [doc1, doc2, doc3];
+const docs = [ doc1 ];
 
 const searchEngine = buildSearchEngine(docs); // поисковый движок запомнил документы
 
 // поиск по документам
-const result = searchEngine.search('shoot');
+// const result = searchEngine.search('shoot');
+const result = searchEngine.search('pint!');
 
 console.log(result);
