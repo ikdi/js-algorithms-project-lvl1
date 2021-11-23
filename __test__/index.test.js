bf9111098d25e64453c1497d54f69bcd0863e822
KDI <perecd@ukr.net>
2021-11-23 15:29:15 +0200

    step 4

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,7 +1,7 @@
 import { test, expect } from '@jest/globals';
-import * as url from 'url';
-import * as path from 'path';
-import * as fs from 'fs';
+// import * as url from 'url';
+// import * as path from 'path';
+// import * as fs from 'fs';
 
 import buildSearchEngine from '../src/index.js';
 
@@ -9,6 +9,14 @@ import buildSearchEngine from '../src/index.js';
 // const __dirname = path.dirname(__filename);
 // const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 // const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+// test('search with term processing', () => {
+//   const doc1 = { id: 'doc1', text: "I can't shoot straight unless I've had a pint!" };
+//   const docs = [doc1];
+
+//   const searchEngine = buildSearchEngine(docs);
+//   expect(searchEngine.search('pint')).toEqual(['doc1']);
+//   expect(searchEngine.search('pint!')).toEqual(['doc1']);
+// });
 
 test('relevant search', () => {
   const doc1 = { id: 'doc1', text: "I can't shoot straight unless I've had a pint!" };
@@ -18,16 +26,6 @@ test('relevant search', () => {
 
   const searchEngine = buildSearchEngine(docs);
 
-  searchEngine.search('shoot');
-
   expect(searchEngine.search('shoot')).toEqual(['doc2', 'doc1']);
-});
-
-test('search with term processing', () => {
-  const doc1 = { id: 'doc1', text: "I can't shoot straight unless I've had a pint!" };
-  const docs = [doc1];
-
-  const searchEngine = buildSearchEngine(docs);
-  expect(searchEngine.search('pint')).toEqual(['doc1']);
-  expect(searchEngine.search('pint!')).toEqual(['doc1']);
+  expect(searchEngine.search('shoot at me')).toEqual(['doc2', 'doc1']);
 });
--- src/index.js
+++ src/index.js
@@ -1,31 +1,44 @@
 const buildSearchEngine = (documents) => {
-  const unsortedIndex = documents.reduce((acc, document) => {
+  const index = documents.reduce((acc, document) => {
     const { id, text } = document;
     const words = text.match(/\w+/g) ?? [];
     /* eslint-disable no-param-reassign */
     return words.reduce((accWords, word) => {
-      const records = accWords[word] ?? [];
-      const record = records.find((item) => item.id === id);
-      const count = (record?.count ?? 0) + 1;
-      accWords[word] = records
-        .filter((item) => item.id !== id)
-        .concat({ id, count });
-
+      const records = accWords[word] ?? {};
+      accWords[word] = {
+        ...records,
+        [id]: (records[id] ?? 0) + 1,
+      };
       return accWords;
     }, acc);
   }, {});
 
-  const index = Object.entries(unsortedIndex)
-    .reduce((acc, [key, value]) => ({
-      ...acc,
-      [key]: value.sort((a, b) => b.count - a.count),
-    }), {});
-
   return {
-    search(word) {
-      const [term] = word.match(/\w+/g);
-      const items = index[term] ?? [];
-      return items.map(({ id }) => id);
+    search(words) {
+      const lexer = {};
+
+      const terms = words.match(/\w+/g);
+      // eslint-disable-next-line no-restricted-syntax
+      for (const term of terms) {
+        const stats = index[term];
+        if (stats) {
+          // eslint-disable-next-line no-restricted-syntax
+          for (const [docId, count] of Object.entries(stats)) {
+            let document = lexer[docId];
+            if (!document) {
+              document = { id: docId, entries: 0, total: 0 };
+              lexer[docId] = document;
+            }
+            document.entries += 1;
+            document.total += count;
+          }
+        }
+      }
+
+      return Object
+        .values(lexer)
+        .sort((a, b) => b.entries - a.entries || b.total - a.total)
+        .map(({ id }) => id);
     },
   };
 };
