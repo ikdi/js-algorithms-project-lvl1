fa43ad05d4b6c8e67c50a4fd96c0f9944c23c6cd
KDI <perecd@ukr.net>
2021-12-04 20:49:07 +0200

    add debug info

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,14 +1,25 @@
-import { test, expect } from '@jest/globals';
+import { test, expect, beforeAll } from '@jest/globals';
 import buildSearchEngine from '../src/index.js';
 
-test('relevant search', () => {
-  const doc1 = { id: 'doc1', text: "I can't shoot straight unless I've had a pint!" };
-  const doc2 = { id: 'doc2', text: "Don't shoot shoot shoot that thing at me." };
-  const doc3 = { id: 'doc3', text: "I'm your shooter." };
-  const docs = [doc1, doc2, doc3];
+const doc1 = { id: 'doc1', text: "I can't shoot straight unless I've had a pint!" };
+const doc2 = { id: 'doc2', text: "Don't shoot shoot shoot that thing at me." };
+const doc3 = { id: 'doc3', text: "I'm your shooter." };
+const collection = [doc1, doc2, doc3];
 
-  const searchEngine = buildSearchEngine(docs);
+let searchEngine = null;
 
+beforeAll(() => {
+  searchEngine = buildSearchEngine(collection);
+});
+
+test('search one word', () => {
   expect(searchEngine.search('shoot')).toEqual(['doc2', 'doc1']);
+});
+
+test('search a sentence', () => {
   expect(searchEngine.search('shoot at me')).toEqual(['doc2', 'doc1']);
+});
+
+test('search a word that doesnot exist', () => {
+  expect(searchEngine.search('ooooops')).toEqual([]);
 });
--- src/index.js
+++ src/index.js
@@ -1,53 +1,70 @@
-const toDocumentTerms = (collection) => collection.map(({ id, text }) => ({ id, terms: text.match(/\w+/g) ?? [] }));
+import words from 'lodash/words';
+
+const toDocumentTerms = (collection) => collection.map(
+  ({ id, text }) => ({ id, terms: words(text) }),
+);
+const calcTdIdf = (termFrequency, documentSize, collectionSize, termCollectionSize) => (
+  (termFrequency / documentSize) * Math.log10(collectionSize / termCollectionSize)
+);
+
+const buildIndex = (documentTerms) => documentTerms.reduce((acc, { id, terms }) => terms
+  .reduce((accDocument, term) => {
+    const list = accDocument[term] ?? {};
+    // eslint-disable-next-line no-param-reassign
+    accDocument[term] = { ...list, [id]: (list[id] ?? 0) + 1 };
+    return accDocument;
+  }, acc), {});
+
+const countStatistic = (documentTerms) => documentTerms.reduce(
+  (acc, { id, terms }) => ({ ...acc, [id]: terms.length }),
+  {},
+);
 
 const buildSearchEngine = (collection) => {
-  const calcTdIdf = (termFrequency, documentSize, collectionSize, termCollectionSize) => (
-    (termFrequency / documentSize) * Math.log2(collectionSize / termCollectionSize)
-  );
-
   const size = collection.length;
 
   const documentTerms = toDocumentTerms(collection);
   /* eslint-disable no-param-reassign */
-  const reverseIndex = documentTerms.reduce((acc, { id, terms }) => terms
-    .reduce((accDocument, term) => {
-      const list = accDocument[term] ?? {};
-      accDocument[term] = { ...list, [id]: (list[id] ?? 0) + 1 };
-      return accDocument;
-    }, acc), {});
+  const reverseIndex = buildIndex(documentTerms);
+  const statistics = countStatistic(documentTerms);
 
-  const statistics = documentTerms.reduce(
-    (acc, { id, terms }) => ({ ...acc, [id]: terms.length }),
-    {},
-  );
+  // console.dir({ reverseIndex });
+  console.dir({ statistics });
 
   return {
-    search(words) {
-      const lexer = {};
-
-      const terms = words.match(/\w+/g);
+    search(query) {
+      console.log('query:', query);
+      const unsortedResult = {};
+      // const terms = query.match(/\w+/g);
+      const terms = words(query);
       // eslint-disable-next-line no-restricted-syntax
       for (const term of terms) {
         const list = reverseIndex[term];
+
+        console.dir({ query, term, list });
+        if (!list) return [];
+
         const listSize = Object.keys(list).length;
-
-        if (list) {
-          // eslint-disable-next-line no-restricted-syntax
-          for (const [docId, frequency] of Object.entries(list)) {
-            let document = lexer[docId];
-            if (!document) {
-              document = { id: docId, score: 0 };
-              lexer[docId] = document;
-            }
-
-            const score = calcTdIdf(frequency, statistics[docId], size, listSize);
-            document.score += score;
+        // eslint-disable-next-line no-restricted-syntax
+        for (const [docId, frequency] of Object.entries(list)) {
+          let document = unsortedResult[docId];
+          if (!document) {
+            document = { id: docId, score: 0 };
+            unsortedResult[docId] = document;
           }
+
+          const score = calcTdIdf(frequency, statistics[docId], size, listSize);
+          // console.dir({
+          //   docId, frequency, allBytDocs: statistics[docId], size, listSize, score,
+          // });
+          document.score += score;
         }
       }
 
+      console.dir({ query, unsortedResult });
+
       return Object
-        .values(lexer)
+        .values(unsortedResult)
         .sort((a, b) => b.score - a.score)
         .map(({ id }) => id);
     },
